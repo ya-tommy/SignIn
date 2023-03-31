d788f06fd1ac28c3e6d961b938cc47324745234d
ya-tommy <65650249+ya-tommy@users.noreply.github.com>
2023-03-31 19:13:25 -0400

    added index + some edits

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var name = "Click on the \'Log in\' button";
+var i = 0
+var speed = 150;
+function typedEffect() {
+    if (i < name.length) {
+        document.getElementById("typed").innerHTML += name.charAt(i);
+        i++
+        setTimeout(typedEffect, speed);
+    }
+}
+
 // ça s'en vient...
